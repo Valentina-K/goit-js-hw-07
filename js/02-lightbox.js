be7b985f4428c7ebbbe237d5f61ae742641c42f9
Valentina-K <kvalentyna@hotmail.com>
2022-11-25 22:10:32 +0200

    hw-2 is ok

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,51 +2,16 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryBox = document.querySelector('.gallery');
 const items = creatGalleryItems(galleryItems);
-let backdrop;
 
 galleryBox.insertAdjacentHTML('beforeend', items);
-
-galleryBox.addEventListener('click', onGalleryBoxClick);
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {});
 
 function creatGalleryItems(items) {
     return items.map(({ preview, original, description }) => {
-        return `
-        <div class="gallery__item">
+        return `        
             <a class="gallery__link" href="${original}">
                 <img class="gallery__image" src="${preview}" alt="${description}">
-            </a>
-        </div>`;
+            </a>`;
     }).join('');
-}
-
-function onGalleryBoxClick(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
-
-    const img = event.target;
-    showModalWindow(img);    
-}
-
-function showModalWindow(element) {
-    const instance = basicLightbox.create(`
-        <div class="modal">
-            <img class="gallery__image" src="${element.dataset.source}" alt="${element.alt}">        
-        </div>
-    `)
-    backdrop = instance;
-    window.addEventListener('keydown', onEscKeyPress);
-    instance.show();
-}
-
-function onEscKeyPress(even) {
-    if (even.code === 'Escape') {
-        window.removeEventListener('keydown', onEscKeyPress);
-        if (document.querySelector('.basicLightbox') !== null) {
-            backdrop.close();
-        }
-    }
-}
-
-console.log(galleryItems);
+};
